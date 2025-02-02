113dcde3df5e3afd45e62af93852aa66c82cc664
AIfonsoJon <hjon@purdue.edu>
2025-02-02 15:38:10 -0500

    Add files via upload

---
+++ IEEE_EMG_Sample_Code.ino
@@ -0,0 +1,58 @@
+#define pin A0
+int val = 0;
+
+void setup() {
+  
+  Serial.begin(9600);
+  pinMode(pin, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int newVal = 0;
+
+  val = analogRead(A0);
+  val = EMGFilter(val);
+
+  //newVal = map(val, 0, 400, 0, 1);
+
+  Serial.println(val);
+  delay(100);
+
+}
+
+float EMGFilter(float input)
+{
+  float output = input;
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.05159732*z1 - 0.36347401*z2;
+    output = 0.01856301*x + 0.03712602*z1 + 0.01856301*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -0.53945795*z1 - 0.39764934*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - 0.47319594*z1 - 0.70744137*z2;
+    output = 1.00000000*x + 2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  {
+    static float z1, z2; // filter section state
+    float x = output - -1.00211112*z1 - 0.74520226*z2;
+    output = 1.00000000*x + -2.00000000*z1 + 1.00000000*z2;
+    z2 = z1;
+    z1 = x;
+  }
+  return output;
+}
